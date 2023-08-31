53905a8106bac51a01730de56ad213380dc60ab6
stevenTheDeve <steventhedeve@gmail.com>
2023-08-30 21:50:36 -0700

    Added binding.

--- SwiftfulThinkingBootcamp/BindingBootcamp.swift
+++ SwiftfulThinkingBootcamp/BindingBootcamp.swift
@@ -10,22 +10,40 @@ import SwiftUI
 struct BindingBootcamp: View {
     
     @State var backgroundColor: Color = Color.green
+    @State var title: String = "Title"
     
     var body: some View {
         ZStack {
             backgroundColor
                 .edgesIgnoringSafeArea(.all)
-            Button {
-                backgroundColor = Color.orange
-            } label: {
-                Text ("Button")
+            
+            VStack {
+                Text (title)
                     .foregroundColor(.white)
-                    .padding()
-                    .padding(.horizontal)
-                    .background (Color.blue)
-                    .cornerRadius(10)
+                ButtonView(backgroundColor: $backgroundColor, title: $title)
             }
 
+        }
+    }
+}
+
+struct ButtonView: View {
+    @Binding var backgroundColor: Color
+    @State var buttonColor: Color = Color.blue
+    @Binding var title: String
+        
+    var body: some View {
+        Button {
+            backgroundColor = Color.orange
+            buttonColor = Color.pink
+            title = ("NEW TITLE!!!")
+        } label: {
+            Text ("Button")
+                .foregroundColor(.white)
+                .padding()
+                .padding(.horizontal)
+                .background (buttonColor)
+                .cornerRadius(10)
         }
     }
 }
