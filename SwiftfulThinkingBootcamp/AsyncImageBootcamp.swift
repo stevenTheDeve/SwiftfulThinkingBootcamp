867b17646f159843e120d3042ecb431a94329540
stevenTheDeve <steventhedeve@gmail.com>
2023-12-02 16:08:18 -0800

    Asyncing has begun

---
+++ SwiftfulThinkingBootcamp/AsyncImageBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  AsyncImageBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Estevan Hinojosa on 12/2/23.
+//
+
+import SwiftUI
+
+struct AsyncImageBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AsyncImageBootcamp()
+}
