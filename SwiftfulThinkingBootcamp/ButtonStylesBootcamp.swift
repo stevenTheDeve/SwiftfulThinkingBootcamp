bfcc9f52cb47ae945792fd9b636fa537145a468a
stevenTheDeve <steventhedeve@gmail.com>
2023-12-02 22:40:35 -0800

    New button styles

---
+++ SwiftfulThinkingBootcamp/ButtonStylesBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonStylesBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Estevan Hinojosa on 12/2/23.
+//
+
+import SwiftUI
+
+struct ButtonStylesBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonStylesBootcamp()
+}
