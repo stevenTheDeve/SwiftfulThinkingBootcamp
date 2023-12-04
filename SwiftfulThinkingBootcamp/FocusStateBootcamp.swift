57ed7b6a6fc5434c1fb6acb4b302d3244eff30d4
stevenTheDeve <steventhedeve@gmail.com>
2023-12-04 01:05:33 -0800

    Focus State

---
+++ SwiftfulThinkingBootcamp/FocusStateBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  FocusStateBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Estevan Hinojosa on 12/4/23.
+//
+
+import SwiftUI
+
+struct FocusStateBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FocusStateBootcamp()
+}
