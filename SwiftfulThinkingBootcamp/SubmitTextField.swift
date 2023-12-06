132b4549ba62db0f83c9593b30973747701777c5
stevenTheDeve <steventhedeve@gmail.com>
2023-12-06 13:33:54 -0800

    Toolbars

---
+++ SwiftfulThinkingBootcamp/SubmitTextField.swift
@@ -0,0 +1,18 @@
+//
+//  SubmitTextField.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Estevan Hinojosa on 12/5/23.
+//
+
+import SwiftUI
+
+struct SubmitTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SubmitTextField()
+}
---
+++ SwiftfulThinkingBootcamp/ToolbarBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ToolbarBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Estevan Hinojosa on 12/6/23.
+//
+
+import SwiftUI
+
+struct ToolbarBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToolbarBootcamp()
+}
