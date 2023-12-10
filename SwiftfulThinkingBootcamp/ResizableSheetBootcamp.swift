8640f6911595153dc9545edebd2c6825d5450441
stevenTheDeve <steventhedeve@gmail.com>
2023-12-09 17:42:14 -0800

    Sheets

---
+++ SwiftfulThinkingBootcamp/ResizableSheetBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ResizableSheetBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Estevan Hinojosa on 12/9/23.
+//
+
+import SwiftUI
+
+struct ResizableSheetBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResizableSheetBootcamp()
+}
