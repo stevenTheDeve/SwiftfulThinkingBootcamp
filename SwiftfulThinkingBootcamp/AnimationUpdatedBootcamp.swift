67af90e1cbe1e729113829bf7ae804386d7880ce
stevenTheDeve <steventhedeve@gmail.com>
2023-12-14 15:11:29 -0800

    animation updated

---
+++ SwiftfulThinkingBootcamp/AnimationUpdatedBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  AnimationUpdatedBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Estevan Hinojosa on 12/9/23.
+//
+
+import SwiftUI
+
+struct AnimationUpdatedBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimationUpdatedBootcamp()
+}
